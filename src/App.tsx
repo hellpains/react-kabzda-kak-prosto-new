59e0373d32d8fd66abc42616104264bd1ce88edd
hellpains <rustam2004sadulaev@mail.ru>
2022-11-02 14:53:56 +0300

    02 - React кабзда как подробно (копоненты часть 2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log('App rendering')
+    return (
+        <div>
+            This is APP Component
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
 }
+
+const Rating = () => {
+    console.log('Rating rendering')
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+const Star = () => {
+    console.log('Star rendering')
+    return (
+        <div>star</div>
+    )
+}
+
+const Accordion = () => {
+
+    console.log('Accordion rendering')
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+const AccordionTitle = () => {
+    console.log('AccordionTitle rendering')
+    return (
+        <h3>Меню</h3>
+    )
+}
+const AccordionBody = () => {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
 
 export default App;
